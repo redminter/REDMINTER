f7c34d013325df481059eaba203316891a3fb3d1
redminter <oleg.a.fedyshyn@gmail.com>
2022-02-10 12:11:04 +0200

    adding gui class

--- src/com/softserve/project/Game2048.java
+++ src/com/softserve/project/Game2048.java
@@ -2,30 +2,6 @@ package com.softserve.project;
 
 public class Game2048 {
     public static void main(String[] args) {
-        Game g=new Game();
-        g.printArray();
-        for(int i=0; i<3; i++) {
-            g.addNewNumber();
-        }
-        g.printArray();
-        for(int i=0;i<3;i++){
-            g.addNewNumber();
-            g.printArray();
-            g.pushUp();
-            g.printArray();
-            g.addNewNumber();
-            g.printArray();
-            g.pushDown();
-            g.printArray();
-
-            g.addNewNumber();
-            g.printArray();
-            g.pushLeft();
-            g.printArray();
-            g.addNewNumber();
-            g.printArray();
-            g.pushRight();
-            g.printArray();
-        }
+        GUI gui = new GUI();
     }
 }
